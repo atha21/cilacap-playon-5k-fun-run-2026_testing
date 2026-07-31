a34844840ed5fd8692237ce22a45ff7b44b825cc
atha21 <91109270+atha21@users.noreply.github.com>
2026-07-31 15:46:46 +0700

    perubahan final
    sedikit revisi design, peraturan bisnis, qris asli

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const CONFIG_TESTING = {
 const CONFIG_PRODUCTION = {
   SUPABASE_URL: "https://stzyvgwxhrqacyjkbwrm.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0enl2Z3d4aHJxYWN5amtid3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4OTI4MDQsImV4cCI6MjA5OTQ2ODgwNH0.2SNFGFCXGaygTkIaWK8bnsMFwv5gAC4SGtji3XHXH5U",
-  WA_ADMIN: "082177378090"
+  WA_ADMIN: "6282177378090"
 };
 
 // Ambil konfigurasi aktif berdasarkan status IS_PRODUCTION
@@ -52,23 +52,23 @@ function getPromoDetails() {
   // Default Normal
   let type = 'Normal';
   let price = 175000;
-  let qrImage = 'qristest.jpeg';
+  let qrImage = 'qrisfix.jpeg';
   let period = 'REGULER PERIODE';
 
   // Ketentuan di tahun 2026
   if (year === 2026) {
     if (month === 7 && date >= 20 && date <= 31) {
-      type = 'Super Early Bird';
-      price = 150000;
-      qrImage = 'qristest.jpeg';
-      period = 'TERSEDIA 20 JULI - 31 JULI 2026';
-    } else if (month === 8 && date >= 1 && date <= 15) {
       type = 'Early Bird';
       price = 165000;
-      qrImage = 'qristest.jpeg'; // disesuaikan dengan ejaan file di folder assets
-      period = 'TERSEDIA 1 AGUSTUS - 15 AGUSTUS 2026';
+      qrImage = 'qrisfix.jpeg';
+      period = 'TERSEDIA 1 AGUSTUS - 10 AGUSTUS 2026';
+    } else if (month === 8 && date >= 1 && date <= 10) {
+      type = 'Early Bird';
+      price = 165000;
+      qrImage = 'qrisfix.jpeg';
+      period = 'TERSEDIA 1 AGUSTUS - 10 AGUSTUS 2026';
     } else {
-      period = 'TERSEDIA 16 AGUSTUS - 31 AGUSTUS 2026';
+      period = 'TERSEDIA 11 AGUSTUS - 31 AGUSTUS 2026';
     }
   }
 
